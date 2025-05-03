e4ab95871a32fdd3cdecccd8d02f58c1daa9c0f3
Abhiram N <abhiramnksd@gmail.com>
2025-05-03 15:42:28 +0530

    fix: resolved merge conflicts in admin dashboard and change db to atlas

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // MongoDB Connection Function
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/userAuth')
+        const conn = await mongoose.connect('mongodb+srv://hp1abhiram:uWjVcmm4hdCcHlmX@cluster0.u1cttcf.mongodb.net/userAuth?retryWrites=true&w=majority');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
